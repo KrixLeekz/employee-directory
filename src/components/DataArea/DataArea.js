cc599d79ad59091df836423ad2c08817b59c41bc
Chris Lyko <chrislyko@gmail.com>
2021-01-17 09:20:26 -0500

    component didmount and render()

--- src/components/DataArea/DataArea.js
+++ src/components/DataArea/DataArea.js
@@ -5,9 +5,20 @@ import "../styles/DataArea.css";
 class DataArea extends Component {
     state = {
         search: "",
+        order: "descend",
         results: [],
+        filteredResults: [],
         error: ""
     };
+
+    componentDidMount() {
+        API.getNumEmployees().then(results => {
+          this.setState({
+            results: results.data.results,
+            filteredResults: results.data.results
+          });
+        });
+    }
 }
 
 export default DataArea;
